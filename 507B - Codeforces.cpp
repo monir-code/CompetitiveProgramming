623d692c0be0f507f4e4dab93ce78b56b654840a
moniruzzaman-monir <monirulhasann@gmail.com>
2016-05-27 17:00:03 +0600

    Update 507B - Codeforces.cpp

--- 507B - Codeforces.cpp
+++ 507B - Codeforces.cpp
@@ -9,10 +9,6 @@ int main()
     double tmp;
     tmp = sqrt((a-x)* (a-x)+ (b-y)*(b-y));
 
-    //if(tmp % r == 0) cout<<tmp/r<<endl;
-    //tmp = (tmp/r)+1;
-    // cout<<(tmp/r)-1<<endl;
-
     if(a ==x && b == y) cout<<"0"<<endl;
     else if(tmp <= 0) cout<<"1"<<endl;
     else
